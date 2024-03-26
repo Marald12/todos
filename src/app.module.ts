e901f1ed993be55881a808f7749d74499c38cc4e
Marald12 <catiklou@gmail.com>
2024-03-26 21:35:37 +0200

    add connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { mongoConfig } from './config/mongo.config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env'
+		}),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: mongoConfig
+		})
+	],
+	controllers: [],
+	providers: []
 })
 export class AppModule {}
